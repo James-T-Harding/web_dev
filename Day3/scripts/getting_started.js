49029f8405515be3e61da377f350ff52d7fdddfb
jhard <j.harding26@outlook.com>
2023-08-02 16:44:43 +0100

    Day 3

---
+++ Day3/scripts/getting_started.js
@@ -0,0 +1,10 @@
+console.log("John")
+console.log("Smith")
+console.log("Wilbertshire")
+console.log("Hyperion")
+
+const car = "Honda"
+const make = "eM1"
+
+console.log(`My favourite car is ${car} and the make is ${make}`)
+console.log("This is a %c formatted string.", "font-style: Bold; color: orange; font-family: fantasy;background-color: black; padding: 10px")
